60c91459c6a051e98d17586390e413f148d30e7c
Aalyth <d.nikolov7654@gmail.com>
2023-11-04 16:08:45 +0200

    add threaded client

---
+++ client/app.py
@@ -0,0 +1,31 @@
+import requests
+
+import threading
+import time
+
+_SERVER_PORT = 3000
+_NUM_THREADS = 10
+
+def monitor_data(identifier: str) -> None:
+    request_data: dict = {       \
+        'timestamp': 0,          \
+        'device_id': identifier, \
+    }
+
+    while True:
+        request_data['timestamp'] = int(time.time())
+        # NOTE: potentially might need to check if the data submit was successful
+        requests.post(f'https://localhost:{_SERVER_PORT}', data=request_data)
+        time.sleep(10)
+
+if __name__ == '__main__':
+    threads: list = []
+
+    for i in range(_NUM_THREADS):
+        identifier: str = f'monitor_device_{i+1}'
+        threads.append( \
+                threading.Thread(target=monitor_data, args=[identifier]) \
+            ) 
+
+    for t in threads:
+        t.start()
